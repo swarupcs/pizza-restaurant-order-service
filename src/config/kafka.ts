671db48c3e337b7eeae62ea4dcfc44cd14d9079e
Swarup Das <swarupcs@gmail.com>
2026-02-21 01:45:52 +0000

    fix commit miss kafka.ts file from prior commits (Issue with uncache commited config files)

---
+++ src/config/kafka.ts
@@ -0,0 +1,55 @@
+import { Consumer, EachMessagePayload, Kafka } from "kafkajs";
+import { MessageBroker } from "../types/broker";
+import { handleProductUpdate } from "../productCache/productUpdateHandler";
+
+export class KafkaBroker implements MessageBroker {
+  private consumer: Consumer;
+
+  constructor(clientId: string, brokers: string[]) {
+    const kafka = new Kafka({ clientId, brokers });
+
+    this.consumer = kafka.consumer({ groupId: clientId });
+  }
+
+  /**
+   * Connect the consumer
+   */
+  async connectConsumer() {
+    await this.consumer.connect();
+  }
+
+  /**
+   * Disconnect the consumer
+   */
+  async disconnectConsumer() {
+    await this.consumer.disconnect();
+  }
+
+  async consumeMessage(topics: string[], fromBeginning: boolean = false) {
+    await this.consumer.subscribe({ topics, fromBeginning });
+
+    await this.consumer.run({
+      eachMessage: async ({
+        topic,
+        partition,
+        message,
+      }: EachMessagePayload) => {
+        // Logic to handle incoming messages.
+
+        switch (topic) {
+          case "product":
+            await handleProductUpdate(message.value.toString());
+            return;
+          default:
+            console.log("Doing nothing...");
+        }
+
+        console.log({
+          value: message.value.toString(),
+          topic,
+          partition,
+        });
+      },
+    });
+  }
+}
