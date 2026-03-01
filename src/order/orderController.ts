3cb1d6cc6a0c1d9c7a7b5c170bc10b702f60f6c3
Swarup Das <swarupcs@gmail.com>
2026-03-01 22:56:27 +0000

    calculate discount

--- src/order/orderController.ts
+++ src/order/orderController.ts
@@ -7,6 +7,7 @@ import {
 } from "../types";
 import productCacheModel from "../productCache/productCacheModel";
 import toppingCacheModel from "../toppingCache/toppingCacheModel";
+import couponModel from "../coupon/couponModel";
 
 export class OrderController {
   create = async (req: Request, res: Response) => {
@@ -14,7 +15,21 @@ export class OrderController {
 
     const totalPrice = await this.calculateTotal(req.body.cart);
 
-    return res.json({ totalPrice: totalPrice });
+    let discountPercentage = 0;
+
+    const couponCode = req.body.couponCode;
+    const tenantId = req.body.tenantId;
+
+    if (couponCode) {
+      discountPercentage = await this.getDiscountPercentage(
+        couponCode,
+        tenantId,
+      );
+    }
+
+    const discountAmount = Math.round((totalPrice * discountPercentage) / 100);
+
+    return res.json({ discountAmount: discountAmount });
   };
 
   private calculateTotal = async (cart: CartItem[]) => {
@@ -98,5 +113,25 @@ export class OrderController {
     }
 
     return currentTopping.price;
+  };
+
+  private getDiscountPercentage = async (
+    couponCode: string,
+    tenantId: string,
+  ) => {
+    const code = await couponModel.findOne({ code: couponCode, tenantId });
+
+    if (!code) {
+      return 0;
+    }
+
+    const currentDate = new Date();
+    const couponDate = new Date(code.validUpto);
+
+    if (currentDate <= couponDate) {
+      return code.discount;
+    }
+
+    return 0;
   };
 }
