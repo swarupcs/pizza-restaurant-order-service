d40ee8f6b36d9eab9bf9b3fa7b622cab3ed2e068
Swarup Das <swarupcs@gmail.com>
2026-03-13 01:24:41 +0000

    add payment gateway

--- src/order/orderController.ts
+++ src/order/orderController.ts
@@ -13,8 +13,11 @@ import { OrderStatus, PaymentStatus } from "./orderTypes";
 import idempotencyModel from "../idempotency/idempotencyModel";
 import mongoose from "mongoose";
 import createHttpError from "http-errors";
+import { PaymentGW } from "../payment/paymentTypes";
 
 export class OrderController {
+  constructor(private paymentGw: PaymentGW) {}
+
   create = async (req: Request, res: Response, next: NextFunction) => {
     const {
       cart,
@@ -100,7 +103,19 @@ export class OrderController {
 
     // Payment processing...
 
-    return res.json({ newOrder: newOrder });
+    // todo: Error handling...
+    // todo: add logging
+    const session = await this.paymentGw.createSession({
+      amount: finalTotal,
+      orderId: newOrder[0]._id.toString(),
+      tenantId: tenantId,
+      currency: "inr",
+      idempotenencyKey: idempotencyKey as string,
+    });
+
+    // todo: Update order document -> paymentId -> sessionId
+
+    return res.json({ paymentUrl: session.paymentUrl });
   };
 
   private calculateTotal = async (cart: CartItem[]) => {
