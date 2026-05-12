ed484894ff20afeb0a9041f5e0cda879e278e3a8
Swarup Das <swarupcs@gmail.com>
2026-05-12 00:21:09 +0000

    fix cors with credential

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const app = express();
 //   config.get("frontend.adminUI"),
 // ];
 
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 
